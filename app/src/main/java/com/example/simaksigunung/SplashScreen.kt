9d7ee0708ce8af2cc6b3b5f70111e25bd86401c8
WawanWidiantara <gdwidi13@gmail.com>
2024-05-14 20:00:07 +0700

    + splash, detail_rute photo update, home changes

---
+++ app/src/main/java/com/example/simaksigunung/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.simaksigunung
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 3000L)
+    }
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
